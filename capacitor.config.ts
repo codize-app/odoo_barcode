a08cf6b928ec289084dd3eb6f5f742fe2c0cfb5f
Ignacio Buioli <ibuioli@gmail.com>
2022-07-12 00:32:22 -0300

    feat: better android, pricelist base

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'ar.codize.odoo_barcode',
-  appName: 'odoo-barcode',
+  appName: 'Odoo Barcode',
   webDir: 'dist/odoo-barcode',
   bundledWebRuntime: false
 };
--- src/app/scanner/scanner.component.ts
+++ src/app/scanner/scanner.component.ts
@@ -16,6 +16,8 @@ export class ScannerComponent implements OnInit {
   loading = true;
   loading_product = false;
   product: Product;
+  pricelists: any;
+  pricelist_id = 0;
   data: any;
   local = Globals.local;
   err = '';
@@ -30,11 +32,13 @@ export class ScannerComponent implements OnInit {
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private barcodeScanner: BarcodeScanner) { }
+    private barcodeScanner: BarcodeScanner
+  ) { }
 
   public ngOnInit(): void {
     if (Globals.uid !== '0') {
       this.odoo.uid = Globals.uid;
+      this.getPricelist();
       this.loading = false;
     } else {
       this.odoo.data().subscribe((data: any) => {
@@ -42,7 +46,7 @@ export class ScannerComponent implements OnInit {
         this.odoo.login().subscribe((res: any) => {
           if (res !== false) {
             Globals.uid = res;
-            console.log(Globals.uid);
+            this.getPricelist();
             this.loading = false;
           }
         });
@@ -51,10 +55,25 @@ export class ScannerComponent implements OnInit {
   }
 
   scanProduct(): void {
-    this.barcodeScanner.scan().then(barcodeData => {
+    this.barcodeScanner.scan({
+      prompt: 'Scan Product Barcode',
+      resultDisplayDuration: 0
+    }).then(barcodeData => {
       this.getProduct(barcodeData.text);
     }).catch(err => {
-      alert(err);
+      this.err = err;
+    });
+  }
+
+  getPricelist(): void {
+    this.odoo.searchRead('product.pricelist', [],
+    {fields: ['name']})
+    .subscribe((res: any) => {
+      console.log(res);
+      if (res.length > 0) {
+        this.pricelist_id = res[0].id;
+        this.pricelists = res;
+      }
     });
   }
 
@@ -66,9 +85,9 @@ export class ScannerComponent implements OnInit {
     let fields = [];
 
     if (this.data.server_version_info[0] <= 12) {
-      fields = ['name', 'lst_price', 'image_medium', 'barcode', 'currency_id'];
+      fields = ['name', 'lst_price', 'image_medium', 'currency_id'];
     } else {
-      fields = ['name', 'lst_price', 'image_256', 'barcode', 'currency_id'];
+      fields = ['name', 'lst_price', 'image_256', 'currency_id'];
     }
 
     this.odoo.searchRead('product.product',
@@ -76,19 +95,22 @@ export class ScannerComponent implements OnInit {
     {fields: fields})
     .subscribe((res: any) => {
       this.loading_product = false;
-      console.log(res);
       if (res.length > 0) {
         this.product = {
           name: res[0].name,
           lst_price: res[0].lst_price,
           image: res[0].image_256 || res[0].image_medium,
-          barcode: res[0].barcode,
+          barcode: barcode,
           currency: res[0].currency_id[1]
         };
       } else {
-        this.err = 'Product not found';
+        this.err = 'Product not found, N° ' + barcode;
       }
     });
+  }
+
+  logout(): void {
+    this.router.navigate(['/home']);
   }
 
 }
